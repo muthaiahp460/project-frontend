e0cd6cc462ee99df704a36c23da399eb7d7ecb50
muthaiahp460 <muthaiahp460@gmail.com>
2025-01-31 15:36:44 +0530

    Initial commit with project files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 // vite.config.js
-export default defineConfig({
+import react from '@vitejs/plugin-react';
+
+export default {
+  plugins: [react()],
   server: {
     port: 3000,  // or whichever port you prefer
     host: '0.0.0.0'  // This allows external access
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        'axios',  // Example: Externalize axios if you want to exclude it
+      ]
+    }
   }
-});
+};
