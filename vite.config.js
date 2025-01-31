0c9e044765b1ee014c62618da8d11e5b46571fc6
muthaiahp460 <muthaiahp460@gmail.com>
2025-01-31 10:38:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['axios', 'react', 'react-dom'] // add any problematic module here
+      external: [
+        'axios',  // Example: Externalize axios if you want to exclude it
+        'react',  // Example: Externalize React if needed
+        'react-dom' // Example: Externalize ReactDOM if needed
+      ]
     }
   }
 });
